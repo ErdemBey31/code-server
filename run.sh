2d8db8e50d3bad33f5d3264ad4a3263ae86ae6d1
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-12-02 19:51:48 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 curl -fsSL https://code-server.dev/install.sh | sh
-code-server --bind-addr 0.0.0.0
-cat /root/.config/code-server/config.yaml
+code-server --bind-addr 0.0.0.0 & cat /root/.config/code-server/config.yaml
+
