7d99a256eedfdee0f907825d056635f83b66afd6
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-12-02 20:27:12 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 curl -fsSL https://code-server.dev/install.sh | sh
-code-server --bind-addr 0.0.0.0 && sleep 20 && cat ~/.config/code-server/config.yaml
+code-server --bind-addr 0.0.0.0 --password "Ekral31#"
