6763381289ef712e61c783eb2589b2519cbb7163
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-12-08 20:43:22 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 wget https://downloads.apache.org/zeppelin/zeppelin-0.10.0/zeppelin-0.10.0-bin-all.tgz
 tar xvf zeppelin-0.10.0-bin-all.tgz
-apt install default-jre
+apt install default-jre -y
 cd zeppelin-0.10.0-bin-all
 ./bin/zeppelin.sh start
