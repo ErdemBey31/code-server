1bb61e13f945331f0e766d9b3d8c1c719e340770
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-12-08 19:59:45 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-curl -fsSL https://code-server.dev/install.sh | sh
-export PASSWORD=123456
-
-code-server --bind-addr 0.0.0.0 --auth password
+apt-get install openjdk-8-jdk
+wget https://downloads.apache.org/zeppelin/zeppelin-0.10.0/zeppelin-0.10.0-bin-all.tgz
+tar -xvzf zeppelin-0.10.0-bin-all.tgz
+cd zeppelin-0.10.0-bin-all
+./bin/zeppelin.sh start
