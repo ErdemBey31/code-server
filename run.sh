adad0511b1257b9105fdc932227f86c172987143
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-12-08 20:17:15 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-apt-get install openjdk-8-jdk
+apt-get install openjdk-8-jdk -y
 wget https://downloads.apache.org/zeppelin/zeppelin-0.10.0/zeppelin-0.10.0-bin-all.tgz
 tar -xvzf zeppelin-0.10.0-bin-all.tgz
 cd zeppelin-0.10.0-bin-all
