a54a5ebbb0733672bea550987d2aaa2784791695
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-12-31 10:53:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-wget -fsSL https://code-server.dev/install.sh | sh
+curl -fsSL https://code-server.dev/install.sh | sh
 export PASSWORD=12345
 code-server --host-addr 0.0.0.0 --port 8080 --password 
 
